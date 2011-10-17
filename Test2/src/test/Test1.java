5ff46c5724c0741a193b7ada1f44e734006ae9ab
kuro <kuro@kuro-PC>
2011-10-17 21:29:41 +0900

    66666 dif

--- Test2/src/test/Test1.java
+++ Test2/src/test/Test1.java
@@ -8,6 +8,9 @@ public class Test1 {
 		System.out.println(3333);
 		System.out.println(4444);
 		
-		System.out.println(55555);
+		System.out.println(66666);
+		System.out.println(66666);
+		System.out.println(66666);
+		
 	}
 }
